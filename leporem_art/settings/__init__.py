de79a87762175360e904cc00437cb8879333bea8
jshong0907 <jshong0907@gmail.com>
2023-06-30 12:58:24 +0900

    Chore: develop settings with s3

--- leporem_art/settings/__init__.py
+++ leporem_art/settings/__init__.py
@@ -4,5 +4,7 @@ from .base import *
 
 if os.environ.get('ENV') == 'prod':
     from .production import *
+elif os.environ.get('ENV') == 'dev':
+    from .develop import *
 else:
     from .local import *
