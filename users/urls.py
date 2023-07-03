255f116b193c7c1511c2e1ba3a212a3aa94b1d00
정유정 <yujeongee@jeong-yujeong-ui-MacBookPro.local>
2023-07-04 03:22:45 +0900

    Feature: setup consent-privacy url

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from src.users.views import ConsentPrivacyView
+from users.views import ConsentPrivacyView
 
 urlpatterns = [
     path('v1/consent-privacy/', ConsentPrivacyView.as_view(), name='consent-privacy')
