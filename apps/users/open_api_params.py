4a9180546449c3ddc6730637325dd219f9ba98a9
정유정 <jjugnyu1282@gmail.com>
2023-07-04 16:49:36 +0900

    Linter

--- apps/users/open_api_params.py
+++ apps/users/open_api_params.py
@@ -4,13 +4,13 @@ get_params = [
     openapi.Parameter(
         '1',
         openapi.IN_QUERY,
-        description = '개인정보 동의여부입니다.',
-        type = openapi.TYPE_BOOLEAN,
+        description='개인정보 동의여부입니다.',
+        type=openapi.TYPE_BOOLEAN,
     ),
     openapi.Parameter(
         '2',
         openapi.IN_QUERY,
-        description = '광고성 정보 동의여부입니다.',
-        type = openapi.TYPE_BOOLEAN,
-    )
+        description='광고성 정보 동의여부입니다.',
+        type=openapi.TYPE_BOOLEAN,
+    ),
 ]
