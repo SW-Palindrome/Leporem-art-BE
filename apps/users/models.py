aa1626ad6fc8667d13731370c2aadd22f2e50751
정유정 <jjugnyu1282@gmail.com>
2023-07-07 17:03:30 +0900

    Fix:User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,12 +4,21 @@ from django.utils import timezone
 
 
 class User(AbstractBaseUser):
+    user_id = models.UUIDField(primary_key=True, editable=False)
     nickname = models.CharField(max_length=20, unique=True)
     profile_image = models.CharField(max_length=50, null=True)
     inactive_datetime = models.DateTimeField(null=True)
     is_agree_privacy = models.BooleanField(default=False)
     is_agree_ads = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+    age_range = models.PositiveIntegerField(null=True)
+    gender = models.CharField(null=True, max_length=2)
+    is_seller = models.BooleanField(null=False)
+    is_staff = models.BooleanField(null=False)
+    created_date = models.DateTimeField(default=timezone.now)
+    created_no = models.PositiveIntegerField(null=False)
+    modified_date = models.DateTimeField(auto_now=True)
+    modified_no = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'nickname'
 
