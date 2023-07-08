23ed194841f1295211c2baa0a1dddd25d31cfcd5
정유정 <jjugnyu1282@gmail.com>
2023-07-09 01:49:35 +0900

    Feat: add kakao-login

--- apps/users/repositories.py
+++ apps/users/repositories.py
@@ -25,3 +25,21 @@ class UserRepository:
             return user_oauth_info.user
         except UserOAuthInfo.DoesNotExist:
             return None
+
+    @transaction.atomic
+    def kakao_signup(self, provider, provider_id, nickname):
+        kakao_oauth_info = UserOAuthInfo.objects.create(provider=provider, provider_id=provider_id)
+        kakao_oauth_info.save()
+        user = User.objects.create(nickname=nickname)
+        user.save()
+        return user
+
+    def kakao_signin(self, provider, provider_id):
+        try:
+            kakao_oauth_info = UserOAuthInfo.objects.get(
+                provider=provider,
+                provider_id=provider_id,
+            )
+            return kakao_oauth_info.user
+        except UserOAuthInfo.DoesNotExist:
+            return None
