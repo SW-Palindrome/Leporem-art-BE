fd3d0e8101e8b0a1b63e3a5cc05e7a1779e2a57a
jshong0907 <jshong0907@gmail.com>
2023-07-10 16:01:32 +0900

    Feature: add seller model

---
+++ apps/sellers/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+from apps.users.models import User
+
+
+class Seller(TimeStampedModel):
+    seller_id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='seller')
+    email = models.EmailField(max_length=50, unique=True)
+    temperature = models.FloatField(null=True)
+
+    def __str__(self):
+        return f'[Seller {self.seller_id}]: {self.email}'
