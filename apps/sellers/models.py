54a2c4f9d8ff1d9214e484a2954b748e6166950c
jshong0907 <jshong0907@gmail.com>
2023-07-10 16:34:54 +0900

    Feautre: add email verify model

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -12,3 +12,10 @@ class Seller(TimeStampedModel):
 
     def __str__(self):
         return f'[Seller {self.seller_id}]: {self.email}'
+
+
+class VerifyEmail(TimeStampedModel):
+    verify_email_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='verify_email')
+    email = models.EmailField(max_length=50)
+    verify_code = models.CharField(max_length=4)
