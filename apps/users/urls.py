b95119a8087eb2d3169d7838ad4a868cc7fe49c8
정유정 <jjugnyu1282@gmail.com>
2023-07-10 21:25:59 +0900

    Fix: signup url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,19 +1,13 @@
 from django.urls import path
 
 from apps.users.views import (
-    ConsentPrivacyView,
+    SignUpView,
     GoogleAuthUrlView,
     GoogleLoginView,
-    GoogleSignupView,
-    KakaoSigninView,
-    KakaoSignupView,
 )
 
 urlpatterns = [
-    path('consent-privacy/', ConsentPrivacyView.as_view()),
+    path('signup/kakao', SignUpView.as_view()),
     path('redirect/google', GoogleAuthUrlView.as_view()),
     path('login/google', GoogleLoginView.as_view()),
-    path('signup/google', GoogleSignupView.as_view()),
-    path('login/kakao', KakaoSigninView.as_view()),
-    path('signup/kakao', KakaoSignupView.as_view()),
 ]
