266cc1a757bededb46e2c909e43a8e6455ffba92
홍준식 <jshong0907@gmail.com>
2023-07-11 12:53:52 +0900

    Fix: authentication with palindrome header (#16)

--- apps/users/authentications.py
+++ apps/users/authentications.py
@@ -8,7 +8,19 @@ from utils.auth.kakao import validate_id_token as kakao_validate_id_token
 
 class OIDCAuthentication(BaseAuthentication):
     def authenticate(self, request):
-        id_token = get_authorization_header(request)
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != b'palindrome':
+            return None
+
+        if len(auth) == 1:
+            msg = 'Invalid basic header. No credentials provided.'
+            raise AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = 'Invalid basic header. Credentials string should not contain spaces.'
+            raise AuthenticationFailed(msg)
+
+        id_token = auth[1]
 
         if isinstance(id_token, bytes):
             id_token = id_token.decode('utf-8')
