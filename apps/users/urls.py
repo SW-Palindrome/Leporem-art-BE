9af85217ffe4dfe20d38f59ab24477cb230aedbd
Yujeong Jeong <117520281+yurad12@users.noreply.github.com>
2023-07-11 18:31:53 +0900

    [P-46] Feature/kakao signin (#18)
    * Feat:kakao-login
    
    * Chore: add parameter
    
    * Feat: add kakao-login
    
    * Feat: add  kakao-login
    
    * Feat: add kakao-login url
    
    * Fix: linter
    
    * Fix: Linter
    
    * Fix: signup url
    
    * Fix: kakao_signup view
    
    * Fix: signup service
    
    * Fix: signup repository
    
    * Fix: parameter
    
    * Linter
    
    * Fix: delete signin
    
    * Fix: delete signin
    
    * Chore: update
    
    * Linter
    
    * Linter
    
    * Fix: signup view
    
    * Fix: signup view
    
    * Linter
    
    * Fix: signup service
    
    * Fix: add check_nickname
    
    * Fix: kakaoauth service
    
    * Fix: signup service
    
    * Fix: signup repository
    
    * Fix: signup view
    
    * Linter
    
    * Linter
    
    * Feat: add signup view
    
    * Feat: add signup repository
    
    * Feat: add signup service
    
    * Feat: add signin param
    
    * Feat: add signin url
    
    * Linter
    
    * Linter
    
    ---------
    
    Co-authored-by: 정유정 <jjugnyu1282@gmail.com>

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.users.views import GoogleAuthUrlView, GoogleLoginView, SignUpView
+from apps.users.views import GoogleAuthUrlView, GoogleLoginView, SignInView, SignUpView
 
 urlpatterns = [
     path('signup/kakao', SignUpView.as_view()),
+    path('login/kakao', SignInView.as_view()),
     path('redirect/google', GoogleAuthUrlView.as_view()),
     path('login/google', GoogleLoginView.as_view()),
 ]
