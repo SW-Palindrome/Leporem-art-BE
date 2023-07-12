7516cbe7d82f0c8ec42eca56f1fd50934b71f7fb
jshong0907 <jshong0907@gmail.com>
2023-07-13 00:40:04 +0900

    Feature: validate nickname api

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.urls import path
 
-from apps.users.views import KakaoLogInView, KakaoSignUpView, RemoveUserView
+from apps.users.views import KakaoLogInView, KakaoSignUpView, RemoveUserView, ValidateNicknameView
 
 urlpatterns = [
     path('signup/kakao', KakaoSignUpView.as_view()),
     path('login/kakao', KakaoLogInView.as_view()),
+    path('validate/nickname/<str:nickname>', ValidateNicknameView.as_view()),
 ]
 
 if settings.DEBUG:
