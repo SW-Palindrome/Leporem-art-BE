fad913a3f1c762df92ac955fe47920f91d536271
jshong0907 <jshong0907@gmail.com>
2023-07-12 15:08:09 +0900

    Fix: settings to use s3 bucket

--- leporem_art/settings/develop.py
+++ leporem_art/settings/develop.py
@@ -2,12 +2,7 @@ import os
 
 from .base import *
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-S3_BUCKET_NAME = "leporem-art-be-static"
-STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
-AWS_S3_BUCKET_NAME_STATIC = S3_BUCKET_NAME
-AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % S3_BUCKET_NAME
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+AWS_STORAGE_BUCKET_NAME = 'leporem-art-media-dev'
 
 DEBUG = True
 
