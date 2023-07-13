cb98eb2b5cc92d90b5cfaf16967764c1b269eb7a
jshong0907 <jshong0907@gmail.com>
2023-07-13 14:57:30 +0900

    Feature: check seller permission

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsStaff(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_staff
+
+
+class IsSeller(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_seller
