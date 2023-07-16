220a368cfe7d5559f56918c68d61902ff6d7d7eb
정유정 <jjugnyu1282@gmail.com>
2023-07-16 23:21:37 +0900

    Feat: add loaditemlist service

--- apps/items/services.py
+++ apps/items/services.py
@@ -1,26 +1,27 @@
 from apps.items.repositories import ItemRepository
 
+
 class LoadItemListService:
-    def load_nickname(self, nickname):
+    def load_nickname(self):
         item_repository = ItemRepository()
-        if not item_repository.load_nickname(nickname):
+        if not item_repository.load_nickname():
             return False
-        return item_repository.load_nickname(nickname)
+        return item_repository.load_nickname()
 
-    def load_item(self, title, price):
+    def load_item(self):
         item_repository = ItemRepository()
-        if not item_repository.load_item_list(title, price):
+        if not item_repository.load_item_list():
             return False
-        return item_repository.load_item_list(title, price)
+        return item_repository.load_item_list()
 
-    def load_image(self, image):
+    def load_image(self):
         item_repository = ItemRepository()
-        if not item_repository.load_image(image):
+        if not item_repository.load_image():
             return False
-        return item_repository.load_image(image)
+        return item_repository.load_image()
 
-    def load_likes(self, item):
+    def load_likes(self):
         item_repository = ItemRepository()
-        if not item_repository.load_like(item):
+        if not item_repository.load_like():
             return False
-        return item_repository.load_like(item)
+        return item_repository.load_like()
