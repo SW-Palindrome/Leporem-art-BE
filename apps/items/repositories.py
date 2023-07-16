e697f5d95c3ea2533d70f783ed9d8a962cc53151
정유정 <jjugnyu1282@gmail.com>
2023-07-16 23:23:08 +0900

    Feat: add item repository

--- apps/items/repositories.py
+++ apps/items/repositories.py
@@ -1,5 +1,7 @@
 from urllib.parse import urljoin
+
 from django.db.models import Count
+
 from apps.items.models import Item, ItemImage, Like
 
 
@@ -18,14 +20,18 @@ class ItemRepository:
 
     def load_item_list(self):
         try:
-            item_info = Item.objects.filter(seller__items__isnull=False).order_by('-display_dt').values_list('title', 'price')
+            item_info = (
+                Item.objects.filter(seller__items__isnull=False).order_by('-display_dt').values_list('title', 'price')
+            )
             return item_info
         except Item.DoesNotExist:
             return None
 
     def load_image(self):
         try:
-            image_info = ItemImage.objects.select_related('item').order_by('-item__display_dt').filter(is_thumbnail=True)
+            image_info = (
+                ItemImage.objects.select_related('item').order_by('-item__display_dt').filter(is_thumbnail=True)
+            )
             image_url = [
                 urljoin('https://leporem-art-media-dev.s3.ap-northeast-2.amazonaws.com/items/item_image/', str(image))
                 for image in image_info
@@ -36,7 +42,13 @@ class ItemRepository:
 
     def load_like(self):
         try:
-            likes = Like.objects.select_related('item').values('item').order_by('-item__display_dt').annotate(like_count=Count('item')).values('like_count')
+            likes = (
+                Like.objects.select_related('item')
+                .values('item')
+                .order_by('-item__display_dt')
+                .annotate(like_count=Count('item'))
+                .values('like_count')
+            )
             return likes
         except likes.DoesNotExist:
             return None
