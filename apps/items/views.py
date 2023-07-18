7d319a56c3d623a715587cbf79fc82a4e114fca2
정유정 <jjugnyu1282@gmail.com>
2023-07-18 20:59:45 +0900

    Fix: loaditemlist view

--- apps/items/views.py
+++ apps/items/views.py
@@ -2,35 +2,29 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.items.services import LoadItemListService
-
+from apps.items.repositories import ItemRepository
+from apps.items.serializers import ItemListSerializer
 
 class LoadItemListView(APIView):
     def get(self, request):
-        load_item_list_service = LoadItemListService()
-        page_number = request.GET.get('page')
+        page_number = request.GET.get('page',1)
+        item_repository = ItemRepository()
+        items = item_repository.load_item_list()
+        paginator = Paginator(items, 20)
+
         try:
-            nicknames = load_item_list_service.load_nickname()
-            items = load_item_list_service.load_item()
-            images = load_item_list_service.load_image()
-            likes = load_item_list_service.load_likes()
+            page_number = int(page_number)
+            page_obj = paginator.page(page_number)
             try:
-                paginator_nicknames = Paginator(nicknames, 20)
-                paginator_items = Paginator(items, 20)
-                paginator_images = Paginator(images, 20)
-                paginator_likes = Paginator(likes, 20)
-                page_nicknames = paginator_nicknames.get_page(page_number)
-                page_items = paginator_items.get_page(page_number)
-                page_images = paginator_images.get_page(page_number)
-                page_likes = paginator_likes.get_page(page_number)
+                serializer = ItemListSerializer(page_obj, many=True)
 
                 return Response(
-                    [{'nicknames': page_nicknames, 'items': page_items, 'images': page_images, 'likes': page_likes}],
+                    {'items': serializer.data},
                     status=200,
                 )
-            except PageNotAnInteger:
-                return Response({'message': 'PageNotAnInteger'}, status=400)
-            except EmptyPage:
-                return Response({'message': 'EmptyPage'}, status=400)
-        except Exception as e:
-            return Response({"error": str(e)}, status=500)
+            except Exception as e:
+                return Response({"error": str(e)}, status=500)
+        except PageNotAnInteger:
+            return Response({'message': 'PageNotAnInteger'}, status=400)
+        except EmptyPage:
+            return Response({'message': 'EmptyPage'}, status=400)
