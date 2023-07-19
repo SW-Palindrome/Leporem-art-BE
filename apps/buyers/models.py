5ed4d5d08416104f2c6f8a175b79a1c85da6e57a
jshong0907 <jshong0907@gmail.com>
2023-07-19 16:27:25 +0900

    Fix: buyer model to timestamped

--- apps/buyers/models.py
+++ apps/buyers/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
 from apps.users.models import User
 
 
-class Buyer(models.Model):
+class Buyer(TimeStampedModel):
     buyer_id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='buyer')
 
