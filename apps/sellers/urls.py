27ca6b6b5db3a1a5a3d5451fbb8ae5c1d1210d9b
jshong0907 <jshong0907@gmail.com>
2023-07-20 16:57:34 +0900

    Feature: register seller info url

--- apps/sellers/urls.py
+++ apps/sellers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from apps.sellers.views import (
+    SellerInfoView,
     SellerItemView,
     SellerMyInfoView,
     SellerRegisterView,
@@ -9,6 +10,7 @@ from apps.sellers.views import (
 
 urlpatterns = [
     path('my', SellerMyInfoView.as_view()),
+    path('<str:nickname>', SellerInfoView.as_view()),
     path('register', SellerRegisterView.as_view()),
     path('verify', SellerVerifyView.as_view()),
     path('items', SellerItemView.as_view()),
