cb6564f7c94292502fcf2a48b51ffd60df153989
jshong0907 <jshong0907@gmail.com>
2023-07-23 19:45:39 +0900

    Feature: save order history when ordered

--- apps/orders/repositories.py
+++ apps/orders/repositories.py
@@ -1,18 +1,23 @@
 from django.db import transaction
 
 from apps.items.models import Item
-from apps.orders.models import Order, OrderStatus
+from apps.orders.models import Order, OrderHistory, OrderStatus
 
 
 class OrderRepository:
     @transaction.atomic
     def order(self, buyer_id, item_id):
         item = Item.objects.get(item_id=item_id)
-        Order.objects.create(
+        order_status = OrderStatus.objects.get(status=OrderStatus.Status.ORDERED.value)
+        order = Order.objects.create(
             buyer_id=buyer_id,
             item=item,
-            order_status=OrderStatus.objects.get(status=OrderStatus.Status.ORDERED.value),
+            order_status=order_status,
             price=item.price,
+        )
+        OrderHistory.objects.create(
+            order=order,
+            order_status=order_status,
         )
         item.current_amount -= 1
         item.save()
