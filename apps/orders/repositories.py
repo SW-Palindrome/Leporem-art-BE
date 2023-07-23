dfacbfc4272adeeff7c00240ce6427568116f900
jshong0907 <jshong0907@gmail.com>
2023-07-23 19:49:50 +0900

    Feature: order cancel repository logic

--- apps/orders/repositories.py
+++ apps/orders/repositories.py
@@ -43,3 +43,17 @@ class OrderRepository:
             order=order,
             order_status=order_status,
         )
+
+    @transaction.atomic
+    def cancel(self, order_id):
+        order = Order.objects.get(order_id=order_id)
+        order_status = OrderStatus.objects.get(status=OrderStatus.Status.CANCELED.value)
+        order.order_status = order_status
+        order.save()
+        OrderHistory.objects.create(
+            order=order,
+            order_status=order_status,
+        )
+        item = order.item
+        item.current_amount += 1
+        item.save()
