91a4a1eb003f499fcdd4cd7e2ef292d913ba7c11
jshong0907 <jshong0907@gmail.com>
2023-07-24 14:59:54 +0900

    Feature: buyer my order info api

--- apps/buyers/serializers.py
+++ apps/buyers/serializers.py
@@ -5,3 +5,13 @@ class BuyerInfoSerializer(serializers.Serializer):
     nickname = serializers.CharField(max_length=20)
     profile_image = serializers.ImageField()
     is_seller = serializers.BooleanField()
+
+
+class BuyerMyOrderSerializer(serializers.Serializer):
+    order_id = serializers.IntegerField()
+    item_id = serializers.IntegerField()
+    item_title = serializers.CharField(source='item.title')
+    item_thumbnail_image = serializers.CharField(source='item.thumbnail_image.image.url')
+    price = serializers.IntegerField()
+    ordered_datetime = serializers.DateTimeField()
+    order_status = serializers.CharField(source='get_order_status_display')
