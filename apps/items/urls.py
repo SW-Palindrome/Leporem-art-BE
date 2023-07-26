78639c3ff610f355f23bae79879ef558430c20cd
정유정 <jjugnyu1282@gmail.com>
2023-07-27 02:37:04 +0900

    Feat: add like url

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.items.views import BuyerItemView, FilterItemView
+from apps.items.views import BuyerItemView, FilterItemView, LikeItemView
 
 urlpatterns = [
     path('filter', FilterItemView.as_view()),
     path('buyer/detail', BuyerItemView.as_view()),
+    path('like', LikeItemView.as_view()),
 ]
