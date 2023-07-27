bb61d75ec35b7396042097bc067d7fdef27e51c5
jshong0907 <jshong0907@gmail.com>
2023-07-27 17:12:34 +0900

    Fix: foreignkey name

--- apps/chats/models.py
+++ apps/chats/models.py
@@ -8,14 +8,14 @@ from apps.users.models import User
 
 class ChatRoom(TimeStampedModel):
     chat_room_id = models.AutoField(primary_key=True)
-    buyer_id = models.ForeignKey(Buyer, on_delete=models.PROTECT)
-    seller_id = models.ForeignKey(Seller, on_delete=models.PROTECT)
+    buyer = models.ForeignKey(Buyer, on_delete=models.PROTECT)
+    seller = models.ForeignKey(Seller, on_delete=models.PROTECT)
 
 
 class Message(TimeStampedModel):
     message_id = models.AutoField(primary_key=True)
-    chat_room_id = models.ForeignKey(ChatRoom, on_delete=models.PROTECT, related_name='messages')
-    sender_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.PROTECT, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.PROTECT)
     write_datetime = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
     text = models.CharField(max_length=255)
