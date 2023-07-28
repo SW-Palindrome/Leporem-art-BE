91da5a23089cdf69923557c13cfcbcd9a60b6481
정유정 <jjugnyu1282@gmail.com>
2023-07-28 19:12:31 +0900

    Fix: fix detail/buyer url

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -4,7 +4,7 @@ from apps.items.views import BuyerItemView, FilterItemView, LikeItemView, Seller
 
 urlpatterns = [
     path('filter', FilterItemView.as_view()),
-    path('buyer/detail', BuyerItemView.as_view()),
+    path('detail/buyer', BuyerItemView.as_view()),
     path('like', LikeItemView.as_view()),
     path('detail/seller', SellerItemView.as_view()),
 ]
