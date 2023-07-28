527cbf0326ceb4a442f7b287ac82cb17eeb5eb4d
정유정 <jjugnyu1282@gmail.com>
2023-07-28 12:12:37 +0900

    Fix: get_items

--- apps/items/repositories.py
+++ apps/items/repositories.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 from django.db import transaction
-from django.db.models import Avg, Count, F, Subquery
+from django.db.models import Avg, Case, Count, F, Subquery, When
 from django.db.models.functions import Round
 from django.utils import timezone
 
@@ -107,14 +107,19 @@ class ItemRepository:
         return Item.objects.get(item_id=item_id)
 
     def get_items(self, buyer_id):
-        is_liked = Subquery(Like.objects.filter(buyer_id=buyer_id)[:1])
+        buyer_id = Like.objects.filter(buyer_id=buyer_id)
         search_item = Item.objects.order_by('-display_dt').annotate(
             nickname=F('seller__user__nickname'),
             like_count=Count('likes'),
             avg_rating=Round(Avg('orders__review__rating'), 1),
             time_diff=timezone.now() - F('display_dt'),
-            is_liked=is_liked,
+            buyer_id=Subquery(buyer_id.values('buyer_id')[:1]),
+            is_liked=Case(
+                When(buyer_id__isnull=False, then=True),
+                default=False,
+            ),
         )
+        search_item.values()
         return search_item
 
     def item_detail(self, item_id, buyer_id):
