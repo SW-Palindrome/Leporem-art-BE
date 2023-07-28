fa7e7395f1fd68b6cbf5bd47bfc71c5b0e3d6047
정유정 <jjugnyu1282@gmail.com>
2023-07-28 19:09:49 +0900

    Feat: add detail/seller url

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.items.views import BuyerItemView, FilterItemView, LikeItemView
+from apps.items.views import BuyerItemView, FilterItemView, LikeItemView, SellerItemView
 
 urlpatterns = [
     path('filter', FilterItemView.as_view()),
     path('buyer/detail', BuyerItemView.as_view()),
     path('like', LikeItemView.as_view()),
+    path('detail/seller', SellerItemView.as_view()),
 ]
