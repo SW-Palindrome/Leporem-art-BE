4e58ef1559deb3e75b18260ea9af56879a01b952
정유정 <jjugnyu1282@gmail.com>
2023-07-31 11:45:35 +0900

    Feat: add review serializer

--- apps/orders/serializers.py
+++ apps/orders/serializers.py
@@ -3,3 +3,9 @@ from rest_framework import serializers
 
 class OrderSerializer(serializers.Serializer):
     item_id = serializers.IntegerField()
+
+
+class ReviewSerializer(serializers.Serializer):
+    order_id = serializers.IntegerField()
+    rating = serializers.IntegerField()
+    comment = serializers.CharField(required=False)
