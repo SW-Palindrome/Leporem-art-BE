c48c087c5595d28082aad3e11433b05304968e66
정유정 <jjugnyu1282@gmail.com>
2023-07-31 15:35:49 +0900

    Feat: add viewed url

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from apps.items.views import BuyerItemView, FilterItemView, LikeItemView, SellerItemView
+from apps.items.views import (
+    BuyerItemView,
+    FilterItemView,
+    LikeItemView,
+    SellerItemView,
+    ViewedItemView,
+)
 
 urlpatterns = [
     path('filter', FilterItemView.as_view()),
     path('detail/buyer', BuyerItemView.as_view()),
     path('like', LikeItemView.as_view()),
     path('detail/seller', SellerItemView.as_view()),
+    path('viewed', ViewedItemView.as_view()),
 ]
