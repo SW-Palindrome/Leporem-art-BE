a9986fb865bf34ee1e04a014f06819011c74d00c
jshong0907 <jshong0907@gmail.com>
2023-08-05 01:17:14 +0900

    Feature: is seller response to login

--- apps/users/views.py
+++ apps/users/views.py
@@ -47,7 +47,7 @@ class KakaoLogInView(APIView):
         user = auth_service.login(id_token=request.data.get('id_token'))
         if user is None:
             return Response({'message': 'signin failed'}, status=401)
-        return Response({'user_id': user.user_id}, status=200)
+        return Response({'user_id': user.user_id, 'is_seller': user.is_seller}, status=200)
 
 
 class ValidateNicknameView(APIView):
