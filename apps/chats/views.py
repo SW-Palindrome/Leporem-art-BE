9b63cf798b0c960ddb5a39c90c8877f074bf0320
jshong0907 <jshong0907@gmail.com>
2023-08-06 02:19:35 +0900

    Fix: message create error

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -59,7 +59,7 @@ class MessageCreateView(APIView):
         serializer.is_valid(raise_exception=True)
         try:
             message = MessageService().create(
-                chat_room_uuid=serializer.validated_data.get['chat_room_uuid'],
+                chat_room_uuid=serializer.validated_data['chat_room_uuid'],
                 user_id=request.user.user_id,
                 text=serializer.validated_data.get('text'),
                 image=serializer.validated_data.get('image'),
