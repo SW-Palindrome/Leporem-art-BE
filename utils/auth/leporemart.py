5cc7970a37886c01a85fc378950d42bf2f3e16b3
정유정 <jjugnyu1282@gmail.com>
2023-08-06 20:23:14 +0900

    Feat: add generate access token

---
+++ utils/auth/leporemart.py
@@ -0,0 +1,84 @@
+from datetime import datetime, timedelta
+
+import jwt
+from django.conf import settings
+from rest_framework.exceptions import AuthenticationFailed
+
+from apps.users.repositories import UserRepository
+
+
+def generate_access_token(payload, type):
+    if type == "access":
+        exp = datetime.now() + timedelta(hours=3)
+    elif type == "refresh":
+        exp = datetime.now() + timedelta(days=180)
+    else:
+        raise Exception("Invalid token type.")
+
+    token_payload = {
+        'provider': payload.provider,
+        'email': payload.email,
+        'exp': exp,
+        'iat': datetime.now(),
+    }
+
+    token = jwt.encode(token_payload, settings.JWT_SECRET_KEY, settings.JWT_ALGORITHM).decode('utf-8')
+
+    return token
+
+
+def validate_token(request):
+    try:
+        token = request.headers.get("AUTHORIZATION")
+        if not token:
+            return None
+        decoded = jwt.decode(token, settings.JWT_AUTH.get("JWT_SECRET_KEY"), algorithms=["HS256"])
+        provider = decoded.get("provider")
+        email = decoded.get("email")
+        user = UserRepository().apple_login(provider, email)
+        return user, None
+    except jwt.exceptions.DecodeError:
+        msg = {
+            "message": "잘못된 토큰입니다.",
+            "code": "JWT_403_INVALID_ACCESSTOKEN",
+        }
+        raise AuthenticationFailed(msg)
+    except jwt.ExpiredSignatureError:
+        msg = {
+            "message": "토큰이 만료되었습니다.",
+            "code": "JWT_403_EXPIRED_ACCESSTOKEN",
+        }
+        raise AuthenticationFailed(msg)
+
+
+def refresh_token(request):
+    user_repository = UserRepository()
+    try:
+        refresh_token = request.headers.get('refresh_token')
+        if not refresh_token:
+            msg = {
+                "message": "refresh_token을 보내주세요.",
+                "code": "JWT_400_NOT_FOUND_TOKEN",
+            }
+            raise AuthenticationFailed(msg)
+        decoded = jwt.decode(refresh_token, settings.JWT_AUTH("JWT_SECRET_KEY"), settings.JWT_AUTH("JWT_ALGORITHM"))
+
+        payload = {'provider': decoded.get("provider"), 'email': decoded.get("email")}
+
+        if decoded.get('exp') < int(datetime.now().timestamp()):
+            if user_repository.get_token(request.user.user_id) == refresh_token:
+                access_token = generate_access_token(payload, "access")
+                refresh_token = generate_access_token(payload, "refresh")
+                user_repository.refresh_token(request.user.user_id, refresh_token)
+            else:
+                raise AuthenticationFailed({"message": "토큰이 일치하지 않습니다."})
+        else:
+            access_token = generate_access_token(payload, "access")
+
+        return {"email": decoded.get("email"), "access_token": access_token, "refresh_token": refresh_token}
+    except jwt.exceptions.DecodeError:
+        msg = {
+            "message": "잘못된 토큰입니다.",
+            "code": "JWT_403_INVALID_ACCESSTOKEN",
+        }
+        raise AuthenticationFailed(msg)
