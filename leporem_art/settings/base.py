74cc4c0e29cc92c3f119ceb4faa83ab3c620216b
정유정 <jjugnyu1282@gmail.com>
2023-08-07 23:06:15 +0900

    Fix: fix jwt auth

--- leporem_art/settings/base.py
+++ leporem_art/settings/base.py
@@ -190,6 +190,4 @@ JWT_AUTH = {
     'JWT_SECRET_KEY': SECRET_KEY,
     'JWT_ALGORITHM': 'HS256',
     'JWT_ALLOW_REFRESH': True,
-    'JWT_EXPIRATION_DELTA': timedelta(days=7),
-    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=28),
 }
