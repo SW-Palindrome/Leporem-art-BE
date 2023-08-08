96b56ecd0cb4e1e206c5de32ed26cac3d241ad9e
jshong0907 <jshong0907@gmail.com>
2023-08-08 14:41:05 +0900

    Feature: move lambda production to private subnet

--- leporem_art/settings/production.py
+++ leporem_art/settings/production.py
@@ -3,6 +3,8 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *
 
+AWS_STORAGE_BUCKET_NAME = 'leporem-art-media-dev'
+
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
@@ -11,5 +13,16 @@ sentry_sdk.init(
     dsn="https://0f4e241425e74741a4f8340ab95c4ba9@app.glitchtip.com/3492",
     integrations=[DjangoIntegration()],
     auto_session_tracking=False,
-    traces_sample_rate=0
+    traces_sample_rate=0,
 )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db-leporemart',
+        'USER': 'palindrome',
+        'PASSWORD': 'dikqU8-jyqjac-ruxxyf',
+        'HOST': 'db.leporem.art',
+        'PORT': '3306',
+    }
+}
