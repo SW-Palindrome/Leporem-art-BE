5804740ad9c2754f822cde8e80973b4cda130758
jshong0907 <jshong0907@gmail.com>
2023-09-05 16:52:38 +0900

    Fix: guest item list api remove inactive user items

--- apps/items/repositories.py
+++ apps/items/repositories.py
@@ -203,12 +203,18 @@ class ItemRepository:
         item.save()
 
     def get_guest_items(self):
-        search_item = Item.objects.order_by('-display_dt').annotate(
-            nickname=F('seller__user__nickname'),
-            like_count=Count('likes', distinct=True),
-            avg_rating=Round(Avg('orders__review__rating'), 1),
-            time_diff=timezone.now() - F('display_dt'),
-            is_liked=Exists(Like.objects.filter(item=OuterRef('item_id'), buyer_id=None)),
+        search_item = (
+            Item.objects.filter(
+                seller__user__inactive_datetime__isnull=True,
+            )
+            .order_by('-display_dt')
+            .annotate(
+                nickname=F('seller__user__nickname'),
+                like_count=Count('likes', distinct=True),
+                avg_rating=Round(Avg('orders__review__rating'), 1),
+                time_diff=timezone.now() - F('display_dt'),
+                is_liked=Exists(Like.objects.filter(item=OuterRef('item_id'), buyer_id=None)),
+            )
         )
         return search_item
 
