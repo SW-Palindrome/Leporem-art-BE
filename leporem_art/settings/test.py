22a078a5da072912aaea466dcaf37cb8916bb199
jshong0907 <jshong0907@gmail.com>
2023-09-11 11:17:42 +0900

    Chore: test settings

---
+++ leporem_art/settings/test.py
@@ -0,0 +1,14 @@
+import os
+
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db-leporemart-test',
+        'USER': 'palindrome',
+        'PASSWORD': 'dikqU8-jyqjac-ruxxyf',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
