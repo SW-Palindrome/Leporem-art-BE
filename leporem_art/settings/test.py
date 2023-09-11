e5457da297078ca44e413fd29639847181f0abe3
jshong0907 <jshong0907@gmail.com>
2023-09-11 11:37:31 +0900

    Fix: ci test config with password

--- leporem_art/settings/test.py
+++ leporem_art/settings/test.py
@@ -6,7 +6,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'db-leporemart-test',
-        'USER': 'root',
+        'USER': 'palindrome',
+        'PASSWORD': 'dikqU8-jyqjac-ruxxyf',
         'HOST': 'localhost',
         'PORT': '3306',
     }
