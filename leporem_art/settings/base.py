9a78967e853061605a042389e0086d97bb197cdc
jshong0907 <jshong0907@gmail.com>
2023-09-13 14:14:18 +0900

    Revert "모든 요청 로깅 추가 (#116)"
    This reverts commit f26162fb47d16b5c70535ad7382c70bbaef2b623.

--- leporem_art/settings/base.py
+++ leporem_art/settings/base.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "request.middleware.RequestMiddleware",
 ]
 
 ROOT_URLCONF = "leporem_art.urls"
