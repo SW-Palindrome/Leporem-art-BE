f26162fb47d16b5c70535ad7382c70bbaef2b623
홍준식 <jshong0907@gmail.com>
2023-09-13 14:01:17 +0900

    모든 요청 로깅 추가 (#116)
    * Fix: is_seller bug
    
    * Fix: cd checkout version
    
    * Chore: provisioned concurrency for remove cold start
    
    * Chore: provisioned concurrecny 3 to 1
    
    * Chore: django request logging

--- leporem_art/settings/base.py
+++ leporem_art/settings/base.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "request.middleware.RequestMiddleware",
 ]
 
 ROOT_URLCONF = "leporem_art.urls"
