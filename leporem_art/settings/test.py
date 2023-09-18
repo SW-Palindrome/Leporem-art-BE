13d578e03ebf8b14b0983b0908e6946a673d9a45
jshong0907 <jshong0907@gmail.com>
2023-09-18 15:57:12 +0900

    Fix: use local storage in test

--- leporem_art/settings/test.py
+++ leporem_art/settings/test.py
@@ -13,3 +13,12 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
