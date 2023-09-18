a2252e77d278d01172bc8adc39abe686d1e6980a
jshong0907 <jshong0907@gmail.com>
2023-09-18 14:09:16 +0900

    Text: buyer chat room list test code

--- tests/chats/test_views.py
+++ tests/chats/test_views.py
@@ -8,6 +8,49 @@ from tests.utils import force_login
 
 
 @pytest.mark.django_db
+class TestBuyerChatRoomView:
+    @pytest.fixture
+    def user(self):
+        return UserFactory()
+
+    @pytest.fixture
+    def buyer(self, user):
+        return BuyerFactory(user=user)
+
+    @pytest.fixture
+    def chat_room_1(self, buyer):
+        return ChatRoomFactory(buyer=buyer)
+
+    @pytest.fixture
+    def chat_room_2(self, buyer):
+        return ChatRoomFactory(buyer=buyer)
+
+    def test_get_chat_room_list(self, client, buyer, user, chat_room_1, chat_room_2):
+        force_login(client, user)
+        response = client.get('/chats/buyer')
+        assert response.status_code == 200
+        data = response.json()
+
+        assert len(data) == 2
+        assert data[0]['chat_room_id']
+        assert data[0]['opponent_nickname']
+        assert data[0]['opponent_user_id']
+        assert data[0]['opponent_profile_image']
+        assert data[0]['message_list']
+        assert data[0]['uuid']
+
+        message_list = data[0]['message_list']
+        assert len(message_list) == 1
+        assert message_list[0]['message_id']
+        assert message_list[0]['user_id']
+        assert message_list[0]['write_datetime']
+        assert not message_list[0]['is_read']
+        assert message_list[0]['message']
+        assert message_list[0]['uuid']
+        assert message_list[0]['type']
+
+
+@pytest.mark.django_db
 class TestSellerChatRoomListView:
     @pytest.fixture
     def user(self):
