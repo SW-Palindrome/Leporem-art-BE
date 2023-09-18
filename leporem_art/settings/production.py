60f1692542a936a39e896ca6391436fad8443b5f
정유정 <jjugnyu1282@gmail.com>
2023-09-18 13:49:47 +0900

    Fix: switched settings to json formatted string

--- leporem_art/settings/production.py
+++ leporem_art/settings/production.py
@@ -27,12 +27,30 @@ SECRET_KEY = ssm.get_parameter(Name='/leporem_art/settings/base/SECRET_KEY', Wit
 param_db = ssm.get_parameter(Name='/leporem_art/settings/production/DATABASES', WithDecryption=True)['Parameter'][
     'Value'
 ]
-DATABASES = {'default': {}}
-[DATABASES['default'].setdefault(i.split(':')[0], i.split(':')[1]) for i in param_db.split('\n') if i != '']
+json_db = json.loads(param_db)
+DATABASES = {
+    'default': {
+        'ENGINE': json_db['ENGINE'],
+        'NAME': json_db['NAME'],
+        'USER': json_db['USER'],
+        'PASSWORD': json_db['PASSWORD'],
+        'HOST': json_db['HOST'],
+        'PORT': json_db['PORT'],
+    }
+}
 
 # APPLE Oauth Settings
 param_apple = ssm.get_parameter(Name='/leporem_art/settings/base/APPLE_CONFIG', WithDecryption=True)['Parameter'][
     'Value'
 ]
-APPLE_CONFIG = {}
-[APPLE_CONFIG.setdefault(i.split(':')[0], i.split(':')[1]) for i in param_apple.split(',\n') if i != '']
+json_apple = json.loads(param_apple)
+APPLE_CONFIG = {
+    'SOCIAL_AUTH_APPLE_ID_CLIENT': json_apple['SOCIAL_AUTH_APPLE_ID_CLIENT'],
+    'SOCIAL_AUTH_APPLE_ID_SERVICE': json_apple['SOCIAL_AUTH_APPLE_ID_SERVICE'],
+    'SOCIAL_AUTH_APPLE_ID_TEAM': json_apple['SOCIAL_AUTH_APPLE_ID_TEAM'],
+    'SOCIAL_AUTH_APPLE_ID_KEY': json_apple['SOCIAL_AUTH_APPLE_ID_KEY'],
+    'SOCIAL_AUTH_APPLE_ID_SECRET': json_apple['SOCIAL_AUTH_APPLE_ID_SECRET'],
+    'SOCIAL_AUTH_APPLE_ID_SCOPE': json_apple['SOCIAL_AUTH_APPLE_ID_SCOPE'],
+    'SOCIAL_AUTH_APPLE_ID_EMAIL_AS_USERNAME': json_apple['SOCIAL_AUTH_APPLE_ID_EMAIL_AS_USERNAME'],
+    'SOCIAL_AUTH_APPLE_ID_PUBLIC': json_apple['SOCIAL_AUTH_APPLE_ID_PUBLIC'],
+}
