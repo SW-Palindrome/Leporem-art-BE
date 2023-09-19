935b9cc0f285f45a2a9b54502af4a7e747925a33
정유정 <jjugnyu1282@gmail.com>
2023-09-19 15:50:35 +0900

    Feat: add specific url

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.notifications.views import DeviceRegisterView
+from apps.notifications.views import DeviceRegisterView, SpecificNotificationView
 
 urlpatterns = [
     path('register', DeviceRegisterView.as_view()),
+    path('specific', SpecificNotificationView.as_view()),
 ]
