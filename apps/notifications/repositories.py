2d526cdf12bf7c864896d58d365ce2ec010adc80
정유정 <jjugnyu1282@gmail.com>
2023-09-19 15:28:00 +0900

    Feat: add device repository

---
+++ apps/notifications/repositories.py
@@ -0,0 +1,8 @@
+from apps.notifications.models import Device
+from apps.users.models import User
+
+
+class DeviceRepository:
+    def register(self, user_id, fcm_token):
+        user = User.objects.get(user_id=user_id)
+        return Device.objects.create(user=user, fcm_token=fcm_token)
