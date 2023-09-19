d00b11e4dd9011681957852e78ea73586657b9e7
정유정 <jjugnyu1282@gmail.com>
2023-09-19 15:51:04 +0900

    Feat: add multiple url

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from apps.notifications.views import DeviceRegisterView, SpecificNotificationView
+from apps.notifications.views import (
+    DeviceRegisterView,
+    MultipleNotificationView,
+    SpecificNotificationView,
+)
 
 urlpatterns = [
     path('register', DeviceRegisterView.as_view()),
     path('specific', SpecificNotificationView.as_view()),
+    path('multiple', MultipleNotificationView.as_view()),
 ]
