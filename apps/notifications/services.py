18a46cf90b098a2bdf50749bd060da51fb247f6f
정유정 <jjugnyu1282@gmail.com>
2023-09-25 16:04:50 +0900

    Fix: register device

--- apps/notifications/services.py
+++ apps/notifications/services.py
@@ -10,6 +10,10 @@ from apps.users.models import User
 class NotificationService:
     def register_device(self, user, fcm_token):
         device_repository = DeviceRepository()
+        if device_repository.get_devices(user, fcm_token):
+            return True
+        if device_repository.get_devices_by_token(fcm_token):
+            device_repository.delete_device(fcm_token)
         device_repository.register(user, fcm_token)
 
     def send_to_specific_device(self, token, title, body, deep_link):
