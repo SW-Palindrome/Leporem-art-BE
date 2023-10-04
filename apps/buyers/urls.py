3d7687bb163190053803f3fdf02c90f27eb100b3
정유정 <jjugnyu1282@gmail.com>
2023-10-05 02:34:53 +0900

    Feat: add exhibition url

--- apps/buyers/urls.py
+++ apps/buyers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.buyers.views import BuyerMyInfoView, BuyerMyOrderView
+from apps.buyers.views import BuyerExhibitionView, BuyerMyInfoView, BuyerMyOrderView
 
 urlpatterns = [
     path('info', BuyerMyInfoView.as_view()),
     path('orders/my', BuyerMyOrderView.as_view()),
+    path('exhibitions', BuyerExhibitionView.as_view()),
 ]
