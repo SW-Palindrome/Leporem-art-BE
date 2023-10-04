94ee1e1a613e4861c877b531b2dec9813901307b
jshong0907 <jshong0907@gmail.com>
2023-10-04 16:40:28 +0900

    Fix: validate user id

--- apps/delivery/services.py
+++ apps/delivery/services.py
@@ -1,8 +1,12 @@
 from apps.delivery.repositories import DeliveryRepository
+from apps.orders.repositories import OrderRepository
 
 
 class DeliveryService:
-    def register(self, order_id: int, delivery_company_name: str, invoice_number: str):
+    def register(self, user_id: int, order_id: int, delivery_company_name: str, invoice_number: str):
+        if OrderRepository().get_order(order_id).item.seller.user_id != user_id:
+            raise ValueError('등록한 작품의 판매자가 아닙니다.')
+
         return DeliveryRepository().register(
             order_id=order_id,
             delivery_company_name=delivery_company_name,
