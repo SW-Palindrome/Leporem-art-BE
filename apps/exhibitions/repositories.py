1226f038216294fe1129d24c568c79d6c4be0f4b
정유정 <jjugnyu1282@gmail.com>
2023-10-06 16:43:39 +0900

    Feat: add modify introduction

--- apps/exhibitions/repositories.py
+++ apps/exhibitions/repositories.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import PermissionDenied
 from django.db import transaction
 
 from apps.exhibitions.models import Exhibition
@@ -12,6 +13,19 @@ class ExhibitionRepository:
             seller=seller,
             start_date=start_date,
             end_date=end_date,
-            artist_name=seller.user.nickname,
         )
+        return exhibition
+
+    @transaction.atomic
+    def modify_introduction(self, seller_id, exhibition_id, cover_image, title, artist_name):
+        try:
+            exhibition = Seller.objects.get(seller_id=seller_id).exhibitions.get(exhibition_id=exhibition_id)
+        except Exhibition.DoesNotExist:
+            raise PermissionDenied
+
+        exhibition.cover_image = cover_image
+        exhibition.title = title
+        exhibition.artist_name = artist_name
+        exhibition.save()
+
         return exhibition
