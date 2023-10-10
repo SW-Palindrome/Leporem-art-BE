c2b71ad8ab5cac512628b3d6ab58765bfe0d178b
정유정 <jjugnyu1282@gmail.com>
2023-10-11 01:19:58 +0900

    Feat: add exhibition introduction view

--- apps/exhibitions/views.py
+++ apps/exhibitions/views.py
@@ -1,3 +1,6 @@
+import random
+
+from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -5,10 +8,12 @@ from rest_framework.views import APIView
 from apps.exhibitions.repositories import ExhibitionRepository
 from apps.exhibitions.serializers import (
     ExhibitionArtistRegisterSerializer,
+    ExhibitionIntroductionSerializer,
     ExhibitionSerializer,
+    ExhibitionsSerializer,
 )
 from apps.exhibitions.services import ExhibitionService
-from apps.users.permissions import IsExhibitionOwner, IsStaff
+from apps.users.permissions import IsExhibitionOwner, IsSeller, IsStaff
 
 
 class ExhibitionView(APIView):
@@ -24,6 +29,33 @@ class ExhibitionView(APIView):
                 end_date=serializer.validated_data['end_date'],
             )
         return Response({'message': 'success'}, status=201)
+
+
+class ExhibitionIntroductionView(APIView):
+    permission_classes = [IsExhibitionOwner]
+    serializer_class = ExhibitionIntroductionSerializer
+    parser_classes = [MultiPartParser]
+
+    def get(self, request, *args, **kwargs):
+        exhibition = ExhibitionRepository().get_introduction(kwargs['exhibition_id'])
+        data = self.serializer_class(exhibition).data
+        return Response(data, status=200)
+
+    def patch(self, request, *args, **kwargs):
+        self.check_object_permissions(request, self.get_object())
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            ExhibitionRepository().modify_introduction(
+                seller_id=request.user.seller.seller_id,
+                exhibition_id=kwargs['exhibition_id'],
+                cover_image=serializer.validated_data['cover_image'],
+                title=serializer.validated_data['title'],
+                artist_name=serializer.validated_data['artist_name'],
+            )
+            return Response({'message': 'success'}, status=200)
+
+    def get_object(self):
+        return ExhibitionRepository().get_exhibition(self.kwargs['exhibition_id'])
 
 
 class ExhibitionArtistView(APIView):
@@ -47,3 +79,26 @@ class ExhibitionArtistView(APIView):
 
     def get_object(self):
         return ExhibitionRepository().get_exhibition(self.kwargs['exhibition_id'])
+
+
+class BuyerExhibitionsView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ExhibitionsSerializer
+
+    def get(self, request):
+        exhibition_repository = ExhibitionRepository()
+        exhibitions = exhibition_repository.get_exhibitions_for_buyer()
+        data = self.serializer_class(exhibitions, many=True).data
+        random.shuffle(data)
+        return Response(data, status=200)
+
+
+class SellerExhibitionsView(APIView):
+    permission_classes = [IsSeller]
+    serializer_class = ExhibitionsSerializer
+
+    def get(self, request):
+        exhibition_repository = ExhibitionRepository()
+        exhibitions = exhibition_repository.get_exhibitions_for_seller(request.user.seller.seller_id)
+        data = self.serializer_class(exhibitions, many=True).data
+        return Response(data, status=200)
