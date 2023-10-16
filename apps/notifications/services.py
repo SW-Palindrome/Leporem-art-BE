fe668eb6bb52d725cf9b84aee4700dd20edc3c88
jshong0907 <jshong0907@gmail.com>
2023-10-16 13:43:58 +0900

    Hotfix: fcm push alarm use async

--- apps/notifications/services.py
+++ apps/notifications/services.py
@@ -1,9 +1,7 @@
+import json
 import logging
 
-import requests
-from django.conf import settings
-from google.auth.transport import requests as auth_requests
-from google.oauth2 import service_account
+import boto3
 
 from apps.notifications.repositories import DeviceRepository
 from apps.users.models import User
@@ -21,34 +19,19 @@ class NotificationService:
         device_repository.register(user, fcm_token)
 
     def send_to_specific_device(self, token, title, body, deep_link):
-        credentials = service_account.Credentials.from_service_account_info(
-            settings.FIREBASE_CONFIG, scopes=['https://www.googleapis.com/auth/cloud-platform']
-        )
-        credentials.refresh(auth_requests.Request())
-        response = requests.post(
-            settings.FIREBASE_MESSAGE_SEND_URL,
-            json={
-                'validate_only': False,
-                'message': {
-                    'data': {
-                        "url": deep_link,
-                    },
-                    'notification': {
-                        'title': title,
-                        'body': body,
-                    },
+        lambda_client = boto3.client('lambda', region_name='ap-northeast-2')
+        lambda_client.invoke(
+            FunctionName='fcmPushAlarm',
+            InvocationType='Event',
+            Payload=json.dumps(
+                {
                     'token': token,
-                    'webpush': {},
-                    'fcm_options': {},
-                },
-            },
-            headers={
-                'Authorization': f'Bearer {credentials.token}',
-            },
+                    'title': title,
+                    'body': body,
+                    'deep_link': deep_link,
+                }
+            ),
         )
-        # FCM Token 중복 시 404 응답
-        if response.status_code not in {200, 404}:
-            logger.error(f'Failed to send notification to {token} with status code {response.status_code}')
 
     def send(self, user: User, title: str, body: str, deep_link: str):
         devices = DeviceRepository().get_devices_by_user(user)
