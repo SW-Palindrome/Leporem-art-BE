2651cd53ccf7e9589f67939e0b19938995faa885
정유정 <jjugnyu1282@gmail.com>
2023-10-16 15:00:19 +0900

    exhibition item url

--- apps/exhibitions/urls.py
+++ apps/exhibitions/urls.py
@@ -4,14 +4,18 @@ from apps.exhibitions.views import (
     BuyerExhibitionsView,
     ExhibitionArtistView,
     ExhibitionIntroductionView,
+    ExhibitionItemView,
     ExhibitionView,
     SellerExhibitionsView,
+    UploadSoundUrlView,
 )
 
 urlpatterns = [
     path('register', ExhibitionView.as_view()),
     path('<int:exhibition_id>/artist-info', ExhibitionArtistView.as_view()),
-    path('introduction/<int:exhibition_id>', ExhibitionIntroductionView.as_view()),
+    path('<int:exhibition_id>/introduction', ExhibitionIntroductionView.as_view()),
     path('buyer', BuyerExhibitionsView.as_view()),
     path('seller', SellerExhibitionsView.as_view()),
+    path('<int:exhibition_id>/register-item', ExhibitionItemView.as_view()),
+    path('sound/upload-url', UploadSoundUrlView.as_view()),
 ]
